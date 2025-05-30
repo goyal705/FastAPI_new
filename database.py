7a346f216fd38624358c3252fac588539c9ea93a
Tushar Goyal <tushargoyal@primuspartners.in>
2025-05-30 09:51:22 +0530

    fix

--- database.py
+++ database.py
@@ -4,9 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-load_dotenv()
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
+# load_dotenv()
+db_pass = os.getenv("db_pass")
+DATABASE_URL = f"postgresql://{db_pass}@pg-893078f-tushargoyal628-b233.d.aivencloud.com:24953/defaultdb?sslmode=require"
+# DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SESSION_LOCAL = sessionmaker(bind=engine,autoflush=False,autocommit=False)
