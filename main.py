81b1ab2c4369fc7916661a89f79fdceda27bb519
Tushar Goyal <tushargoyal@primuspartners.in>
2025-05-31 10:29:28 +0530

    fix

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 from database import *
 from schemas import *
 from models import *
-from routers import blog,user
+from routers import blog,user,commets
 
 app = FastAPI()
 
+app.include_router(commets.router)
 app.include_router(blog.router)
 app.include_router(user.router)
 
--- routers/token.py
+++ routers/token.py
@@ -17,7 +17,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     if expires_delta:
         expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=1)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=30)
 
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
